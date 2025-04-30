1ab85cd1ad7307e8aa6c113e6d1d057ae98eb5f0
Amirali <amiraliesmaeli741@gmail.com>
2025-04-30 14:10:53 +0330

    update gitignore, settings.py, urls.py (if DEBUG for static), and added {% load static %} to base.html

--- planner/urls.py
+++ planner/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
