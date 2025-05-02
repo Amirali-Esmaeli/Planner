f96921239ccd344f230cdb905a0c87b0794bdd4d
Amirali <amiraliesmaeli741@gmail.com>
2025-05-02 15:39:47 +0330

    Add URLs for habit_create , task_create ,goal_create and goal_edit

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login', views.CustomLoginView.as_view(), name='login'),
     path('logout', views.custom_logout, name='logout'),
-    
+    path('goal/edit/<int:goal_id>/', views.goal_edit, name='goal_edit'),
+    path('goal/create/', views.goal_create, name='goal_create'),
+    path('habit/create/', views.habit_create, name='habit_create'),
+    path('task/create/', views.task_create, name='task_create'),
 ]
