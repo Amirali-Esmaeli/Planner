ca8b4cee23c932d03138495d716a4384cff71cb2
Amirali <amiraliesmaeli741@gmail.com>
2025-05-03 23:19:45 +0330

    Add task_form.html template,Add client-side validation for task and fixed bug habit_form.html

---
+++ static/js/task_form.js
@@ -0,0 +1,16 @@
+document.getElementById('task-form').addEventListener('submit', function(e){
+    const title = document.querySelector('input[name="title"]').value;
+    const dueDate = document.querySelector('input[name="due_date"]').value;
+    const today = new Date().toISOString().split('T')[0];
+
+    if (!title.trim()) {
+        alert('عنوان وظیفه نمی‌تواند خالی باشد');
+        e.preventDefault();
+    }else if (!dueDate) {
+        alert('تاریخ سررسید نمی‌تواند خالی باشد');
+        e.preventDefault();
+    }else if (dueDate < today) {
+        alert('تاریخ سررسید نمی‌تواند در گذشته باشد');
+        e.preventDefault();
+    }
+});
